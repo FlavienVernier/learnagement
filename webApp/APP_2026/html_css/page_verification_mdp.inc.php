3a59e4e65ee2ba08377029bc11dd5715c45b5ed7
Flavien <flavien.vernier@gmail.com>
2025-07-07 11:51:03 +0200

    start crud login unification

--- webApp/APP_2026/html_css/page_verification_mdp.inc.php
+++ webApp/APP_2026/html_css/page_verification_mdp.inc.php
@@ -2,12 +2,16 @@
 
 
 <?php
+
+loadEnv("../..");
+
 if( isset( $_POST["inscription_ok"])){
 
   //vérification si personne prof ou etu
   $sql1="SELECT id_etudiant, mail, password, password_updated FROM LNM_etudiant WHERE mail LIKE'".$_POST["id"]."'";
   $result1 = mysqli_query($conn, $sql1) or die("Requête invalide: ". mysqli_error( $conn )."\n".$sql1);
   $val= mysqli_fetch_array($result1);
+
   if ($val){
     //Si étudiant
     //Vérification mdp
@@ -40,12 +44,34 @@ if( isset( $_POST["inscription_ok"])){
       echo "<script>window.location.href='?page=connexion'</script>";
     }
   }
-  else if ($val == NULL){
-    $sql2="SELECT id_enseignant, mail, password, password_updated FROM LNM_enseignant WHERE mail LIKE'".$_POST["id"]."'";
-    $result2 = mysqli_query($conn, $sql2) or die("Requête invalide: ". mysqli_error( $conn )."\n".$sql2);
-    $val= mysqli_fetch_array($result2);
+  else if ($val == NULL) {
+      $sql2="SELECT id_enseignant, mail, password, password_updated FROM LNM_enseignant WHERE mail LIKE'".$_POST["id"]."'";
+      $result2 = mysqli_query($conn, $sql2) or die("Requête invalide: ". mysqli_error( $conn )."\n".$sql2);
+      $val= mysqli_fetch_array($result2);
 
-    if($val){
+      /*$url = $_ENV['PHP_BACKEND_DOCKER_URL'] . "/connection/authenticate.php";
+      $data = ['username' => $_POST["id"], 'password' => $_POST['mdp']];
+
+      // use key 'http' even if you send the request to https://...
+      $options = [
+          'http' => [
+              'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+              'method' => 'POST',
+              'content' => http_build_query($data),
+          ],
+      ];
+
+      $context = stream_context_create($options);
+      $result = file_get_contents($url, false, $context);
+      if ($result === false) {
+          // Handle error
+      }
+
+      var_dump($result);
+  }*/
+
+
+      if($val){
       //Si enseignant
       //Vérification mdp
       if ($val['password_updated'] != NULL && password_verify($_POST['mdp'], $val['password'])){
