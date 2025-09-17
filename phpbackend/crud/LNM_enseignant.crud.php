bd941ce70dd72b68bc9ceb432809bfa4cfe6e424
Flavien <flavien.vernier@gmail.com>
2025-09-17 09:34:42 +0200

    add module responsabilities display

--- phpbackend/crud/LNM_enseignant.crud.php
+++ phpbackend/crud/LNM_enseignant.crud.php
@@ -9,3 +9,24 @@ function listLNM_enseignant($conn)
     $rs = rs_to_table($res);
     return $rs;
 }
+
+function listLNM_enseignant_responsabilite($conn)
+{
+    $sql = "SELECT 
+        CONCAT(`LNM_enseignant`.`prenom`,' ',`LNM_enseignant`.`nom`) AS `responsable`,
+        COUNT(`MAQUETTE_module`.`id_module`) AS `responsabilite`,
+        GROUP_CONCAT(distinct `MAQUETTE_module`.`code_module` separator ', ') AS `modules` 
+        FROM `LNM_enseignant` 
+        JOIN `MAQUETTE_module` ON `MAQUETTE_module`.`id_responsable` = `LNM_enseignant`.`id_enseignant` 
+        JOIN `MAQUETTE_module_as_learning_unit` ON `MAQUETTE_module_as_learning_unit`.`id_module` = `MAQUETTE_module`.`id_module`
+        JOIN `MAQUETTE_learning_unit` ON `MAQUETTE_learning_unit`.`id_learning_unit` = `MAQUETTE_module_as_learning_unit`.`id_learning_unit`
+        JOIN `LNM_promo` ON `MAQUETTE_learning_unit`.`id_promo` = `LNM_promo`.`id_promo`
+        JOIN `LNM_statut` ON `LNM_statut`.`id_statut` = `LNM_promo`.`id_statut`
+        JOIN `LNM_filiere` ON `LNM_filiere`.`id_filiere` = `LNM_promo`.`id_filiere`
+        JOIN `LNM_semestre` ON `LNM_semestre`.`id_semestre` = `MAQUETTE_module`.`id_semestre`
+        JOIN `MAQUETTE_discipline` ON `MAQUETTE_discipline`.`id_discipline` = `MAQUETTE_module`.`id_discipline`
+        GROUP BY `LNM_enseignant`.`nom`, `LNM_enseignant`.`prenom`;";
+    $res = mysqli_query($conn, $sql);
+    $rs = rs_to_table($res);
+    return $rs;
+}
---
+++ phpbackend/list/listEnseignantResponsabilite.php
@@ -0,0 +1,23 @@
+<?php
+    header("Access-Control-Allow-Origin: http://localhost:40081"); // Activer CORS
+    header("Access-Control-Allow-Credentials: true"); // Autoriser le partage de cookies
+
+    header("Content-Type: application/json");
+
+    session_start();
+
+    include("../db_connection/connectDB.php");
+    include("../crud/LNM_enseignant.crud.php");
+    include("../crud/function_rs_to_table.php");
+    include("../crud/function_action_allowed.php");
+
+
+    try{
+        $rsModules = listLNM_enseignant_responsabilite($conn);
+
+        echo json_encode($rsModules);
+    }catch (Exception $e) {
+	    echo json_encode($e->getMessage());
+    }
+
+
