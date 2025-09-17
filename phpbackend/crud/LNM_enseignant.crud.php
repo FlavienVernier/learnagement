3658994ab1f31c2ed22a14b6ae42f46e14efabe6
Flavien <flavien.vernier@gmail.com>
2025-09-17 09:49:26 +0200

    bug correction in  module responsabilities display

--- phpbackend/crud/LNM_enseignant.crud.php
+++ phpbackend/crud/LNM_enseignant.crud.php
@@ -18,13 +18,6 @@ function listLNM_enseignant_responsabilite($conn)
         GROUP_CONCAT(distinct `MAQUETTE_module`.`code_module` separator ', ') AS `modules` 
         FROM `LNM_enseignant` 
         JOIN `MAQUETTE_module` ON `MAQUETTE_module`.`id_responsable` = `LNM_enseignant`.`id_enseignant` 
-        JOIN `MAQUETTE_module_as_learning_unit` ON `MAQUETTE_module_as_learning_unit`.`id_module` = `MAQUETTE_module`.`id_module`
-        JOIN `MAQUETTE_learning_unit` ON `MAQUETTE_learning_unit`.`id_learning_unit` = `MAQUETTE_module_as_learning_unit`.`id_learning_unit`
-        JOIN `LNM_promo` ON `MAQUETTE_learning_unit`.`id_promo` = `LNM_promo`.`id_promo`
-        JOIN `LNM_statut` ON `LNM_statut`.`id_statut` = `LNM_promo`.`id_statut`
-        JOIN `LNM_filiere` ON `LNM_filiere`.`id_filiere` = `LNM_promo`.`id_filiere`
-        JOIN `LNM_semestre` ON `LNM_semestre`.`id_semestre` = `MAQUETTE_module`.`id_semestre`
-        JOIN `MAQUETTE_discipline` ON `MAQUETTE_discipline`.`id_discipline` = `MAQUETTE_module`.`id_discipline`
         GROUP BY `LNM_enseignant`.`nom`, `LNM_enseignant`.`prenom`;";
     $res = mysqli_query($conn, $sql);
     $rs = rs_to_table($res);
