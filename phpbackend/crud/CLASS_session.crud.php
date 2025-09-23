e09e8061547e35414c6ec8e148ef3c88e03130d7
Flavien <flavien.vernier@gmail.com>
2025-09-23 21:25:22 +0200

    move check sql requests from DB (deprecated system) to backend

--- phpbackend/crud/CLASS_session.crud.php
+++ phpbackend/crud/CLASS_session.crud.php
@@ -60,6 +60,30 @@ MAQUETTE_module_sequence.numero_ordre, MAQUETTE_module_sequence.id_intervenant_p
     return $rs;
 }
 
+function checkCLASS_sessionWithoutIntervenant($conn) {
+    $sql = "SELECT CLASS_session.id_groupe, LNM_groupe.nom_groupe, LNM_promo.id_promo, 'ExplicitSecondaryPromoKs not yet generated' as promo,
+MAQUETTE_module_sequence.numero_ordre, MAQUETTE_module_sequence.id_intervenant_principal, MAQUETTE_module_sequence.commentaire,
+                    MAQUETTE_module_sequencage.id_module, MAQUETTE_module_sequencage.id_seance_type, MAQUETTE_module_sequencage.duree_h,
+                    MAQUETTE_module.code_module, MAQUETTE_module.nom as nom_module,
+                    LNM_seanceType.type,
+                    LNM_semestre.semestre
+            FROM CLASS_session
+            	LEFT JOIN LNM_groupe ON LNM_groupe.id_groupe = CLASS_session.id_groupe
+                LEFT JOIN LNM_promo ON LNM_promo.id_promo = LNM_groupe.id_promo
+            	LEFT JOIN MAQUETTE_module_sequence ON MAQUETTE_module_sequence.id_module_sequence = CLASS_session.id_module_sequence
+            	LEFT JOIN MAQUETTE_module_sequencage ON MAQUETTE_module_sequencage.id_module_sequencage = MAQUETTE_module_sequence.id_module_sequencage
+                LEFT JOIN MAQUETTE_module ON MAQUETTE_module.id_module = MAQUETTE_module_sequencage.id_module
+                LEFT JOIN MAQUETTE_module_as_learning_unit ON MAQUETTE_module_as_learning_unit.id_module = MAQUETTE_module.id_module
+                LEFT JOIN MAQUETTE_learning_unit ON MAQUETTE_learning_unit.id_learning_unit = MAQUETTE_module_as_learning_unit.id_learning_unit AND MAQUETTE_learning_unit.id_promo = LNM_promo.id_promo
+                LEFT JOIN LNM_semestre ON LNM_semestre.id_semestre = MAQUETTE_module.id_semestre
+                LEFT JOIN LNM_seanceType ON LNM_seanceType.id_seance_type = MAQUETTE_module_sequencage.id_seance_type
+                LEFT JOIN LNM_groupe_type ON LNM_groupe_type.id_groupe_type = MAQUETTE_module_sequencage.id_groupe_type
+            WHERE CLASS_session.id_enseignant IS NULL;";
+    $res = mysqli_query($conn, $sql);
+    $rs = rs_to_table($res);
+    return $rs;
+}
+
 function setCLASS_session_enseignant($conn, $id_session,  $id_enseignant){
     $sql = "UPDATE CLASS_session
             SET id_enseignant = '$id_enseignant'
--- phpbackend/crud/LNM_enseignant.crud.php
+++ phpbackend/crud/LNM_enseignant.crud.php
@@ -23,3 +23,16 @@ function listLNM_enseignant_responsabilite($conn)
     $rs = rs_to_table($res);
     return $rs;
 }
+
+function checkLNM_enseignant_sans_cours($conn)
+{
+    $sql = "SELECT `prenom`, `nom`, `mail`, `statut`, `composante`
+            FROM LNM_enseignant
+            WHERE LNM_enseignant.id_enseignant 
+                      NOT IN (  SELECT CLASS_session.id_enseignant 
+                                FROM CLASS_session 
+                                WHERE CLASS_session.id_enseignant IS NOT null);";
+    $res = mysqli_query($conn, $sql);
+    $rs = rs_to_table($res);
+    return $rs;
+}
