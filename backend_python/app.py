762408fe8563dc9f272ac47023d11e4d410f43a1
Flavien <flavien.vernier@gmail.com>
2026-01-29 14:05:40 +0100

    bug correction in health dependencies for dockers

--- backend_python/app.py
+++ backend_python/app.py
@@ -1,10 +1,13 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Request
 import uvicorn
+import logging
 
 from system import authenticate, check
 from user import LNM_enseignant
 from user import LNM_university
 from user import APC_competence
+
+access_logger = logging.getLogger("uvicorn.access")
 
 #app = FastAPI(dependencies=[Depends(get_query_token)])
 app = FastAPI()
@@ -23,9 +26,20 @@ app.include_router(APC_competence.router)
 #     responses={418: {"description": "I'm a teapot"}},
 # )
 
+# Classical health
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+# make health silent (without log)
+@app.middleware("http")
+async def silence_health_logs(request: Request, call_next):
+    if request.url.path == "/health":
+        access_logger.disabled = True
+        response = await call_next(request)
+        access_logger.disabled = False
+        return response
+    return await call_next(request)
 @app.get("/")
 async def root():
     return {"message": "Hello, I'm Learnagement BackEnd!"}
