a4eb2505dc84a5a69f0f6ae3929aef41663145a4
Flavien <flavien.vernier@gmail.com>
2026-02-03 11:23:33 +0100

    next step to manage empty data

--- front_DashPlotly/app5_module_enseignant_edit.py
+++ front_DashPlotly/app5_module_enseignant_edit.py
@@ -56,118 +56,126 @@ def update_table_sequencage(user_id, selected_module):
     if not selected_module:
         return []
     df = app5_module_tools.get_moduleSequencageByEnseignantId(user_id)
-    df = df[df['id_module'] == selected_module][['nombre', 'type', 'duree_h', 'groupe_type', 'intervenant_principal']]
-    #df = df[df['id_module'] == selected_module]
-    #data=df.to_dict('records')
-    #print(data, flush=True)
-    dfi = app_tools.get_explicit_keys("LNM_enseignant")
-    intervenant_options = [{'label': row['ExplicitSecondaryK'], 'value': row['id']} for _, row in dfi.iterrows()]
+    if not df.empty:
+        df = df[df['id_module'] == selected_module][['nombre', 'type', 'duree_h', 'groupe_type', 'intervenant_principal']]
+        #df = df[df['id_module'] == selected_module]
+        #data=df.to_dict('records')
+        #print(data, flush=True)
+        dfi = app_tools.get_explicit_keys("LNM_enseignant")
+        intervenant_options = [{'label': row['ExplicitSecondaryK'], 'value': row['id']} for _, row in dfi.iterrows()]
 
-    table_sequencage = dash_table.DataTable(
-        id='table_sequencage',
-        # columns=[{"name": i, "id": i}
-        #          if i != 'intervenant_principal'
-        #          else {"name": i, "id": i, "editable": True, "presentation": "dropdown",}
-        #          for i in df.columns],  # columns must be defined so that DataTable be editable
-        columns=[{"name": i, "id": i}
-                 #if i != 'intervenant_principal'
-                 #else {"name": i, "id": i, "editable": True, "presentation": "dropdown", }
-                 for i in df.columns] + [{"name": 'nouvel_intervenant', "id": 'nouvel_intervenant', "editable": True, "presentation": "dropdown", }],  # columns must be defined so that DataTable be editable
-        data=df.to_dict('records'),
-        editable=True,
-        row_deletable=True,
-        dropdown={
-            #"intervenant_principal": {
-            "nouvel_intervenant": {
-                "options": intervenant_options,
-                "clearable":True,
-            }
-        },
-    )
+        table_sequencage = dash_table.DataTable(
+            id='table_sequencage',
+            # columns=[{"name": i, "id": i}
+            #          if i != 'intervenant_principal'
+            #          else {"name": i, "id": i, "editable": True, "presentation": "dropdown",}
+            #          for i in df.columns],  # columns must be defined so that DataTable be editable
+            columns=[{"name": i, "id": i}
+                     #if i != 'intervenant_principal'
+                     #else {"name": i, "id": i, "editable": True, "presentation": "dropdown", }
+                     for i in df.columns] + [{"name": 'nouvel_intervenant', "id": 'nouvel_intervenant', "editable": True, "presentation": "dropdown", }],  # columns must be defined so that DataTable be editable
+            data=df.to_dict('records'),
+            editable=True,
+            row_deletable=True,
+            dropdown={
+                #"intervenant_principal": {
+                "nouvel_intervenant": {
+                    "options": intervenant_options,
+                    "clearable":True,
+                }
+            },
+        )
+    else:
+        table_sequencage = dash_table.DataTable()
     return [table_sequencage]
 
 def update_table_sequence(user_id, selected_module, selected_seance_type):
     if not selected_module:
         return []
     df = app5_module_tools.get_moduleSequenceByEnseignantId(user_id)
-    if selected_seance_type:
-        df = df[(df['id_module'] == selected_module) & (df['id_seance_type'] == selected_seance_type)][['id_sequence', 'type', 'duree_h', 'groupe_type', 'numero_ordre', 'intervenant_principal', 'commentaire']]
-    else: #display all sequence types
-        df = df[df['id_module'] == selected_module][['id_sequence', 'type', 'numero_ordre', 'duree_h', 'groupe_type', 'intervenant_principal', 'commentaire']]
+    if not df.empty:
+        if selected_seance_type:
+            df = df[(df['id_module'] == selected_module) & (df['id_seance_type'] == selected_seance_type)][['id_sequence', 'type', 'duree_h', 'groupe_type', 'numero_ordre', 'intervenant_principal', 'commentaire']]
+        else: #display all sequence types
+            df = df[df['id_module'] == selected_module][['id_sequence', 'type', 'numero_ordre', 'duree_h', 'groupe_type', 'intervenant_principal', 'commentaire']]
 
-    dfi = app_tools.get_explicit_keys("LNM_enseignant")
-    intervenant_options = [{'label': row['ExplicitSecondaryK'], 'value': row['id']} for _, row in dfi.iterrows()]
+        dfi = app_tools.get_explicit_keys("LNM_enseignant")
+        intervenant_options = [{'label': row['ExplicitSecondaryK'], 'value': row['id']} for _, row in dfi.iterrows()]
 
-    table_sequence = dash_table.DataTable(
-        id='table_sequence',
-        columns=[{"name": i, "id": i}
-                 for i in df.columns] + [{"name": 'nouvel_intervenant', "id": 'nouvel_intervenant', "editable": True, "presentation": "dropdown", }],  # columns must be defined so that DataTable be editable
-        editable=True,
-        data=df.to_dict('records'),
-        row_deletable=False,
-        dropdown={
-            "nouvel_intervenant": {
-                "options": intervenant_options,
-                "clearable":True,
-            }
-        },
-        # style_cell_conditional=[
-        #     {'if': {'column_id': 'id_sequence', },
-        #         'display': 'none'
-        #     }
-        # ],
-        # style_header_conditional=[
-        #     {'if': {'column_id': 'id_sequence', },
-        #      'display': 'None'
-        #      }
-        # ],
-        style_cell_conditional=[
-            {'if': {'column_id': 'id_sequence', },
-             'display': 'None', }
-        ],
-    )
+        table_sequence = dash_table.DataTable(
+            id='table_sequence',
+            columns=[{"name": i, "id": i}
+                     for i in df.columns] + [{"name": 'nouvel_intervenant', "id": 'nouvel_intervenant', "editable": True, "presentation": "dropdown", }],  # columns must be defined so that DataTable be editable
+            editable=True,
+            data=df.to_dict('records'),
+            row_deletable=False,
+            dropdown={
+                "nouvel_intervenant": {
+                    "options": intervenant_options,
+                    "clearable":True,
+                }
+            },
+            # style_cell_conditional=[
+            #     {'if': {'column_id': 'id_sequence', },
+            #         'display': 'none'
+            #     }
+            # ],
+            # style_header_conditional=[
+            #     {'if': {'column_id': 'id_sequence', },
+            #      'display': 'None'
+            #      }
+            # ],
+            style_cell_conditional=[
+                {'if': {'column_id': 'id_sequence', },
+                 'display': 'None', }
+            ],
+        )
+    else:
+        table_sequence = dash_table.DataTable()
     return [table_sequence]
 
 def update_table_session(user_id, selected_module, selected_seance_type, selected_promotion):
     if not selected_module:
         return []
     df = app5_module_tools.get_moduleSessionByEnseignantId(user_id)
+    if not df.empty:
+        if selected_seance_type and selected_promotion:
+            df = df[(df['id_module'] == selected_module) &
+                    (df['id_seance_type'] == selected_seance_type) &
+                    (df['id_promo'] == selected_promotion)][['id_session', 'type', 'duree_h', 'nom_groupe', 'numero_ordre', 'intervenant', 'commentaire']].sort_values(by=['type', 'numero_ordre'], ascending=[False, False])
+        elif selected_seance_type:
+            df = df[(df['id_module'] == selected_module) &
+                    (df['id_seance_type'] == selected_seance_type)][['id_session', 'type', 'numero_ordre', 'duree_h', 'nom_groupe', 'intervenant', 'commentaire']].sort_values(by=['type', 'numero_ordre'], ascending=[False, False])
+        elif selected_promotion:
+            df = df[(df['id_module'] == selected_module) &
+                    (df['id_promo'] == selected_promotion)][['id_session', 'type', 'duree_h', 'nom_groupe', 'numero_ordre', 'intervenant', 'commentaire']].sort_values(by=['type', 'numero_ordre'], ascending=[False, False])
+        else:
+            df = df[(df['id_module'] == selected_module)][
+                ['id_session', 'type', 'numero_ordre', 'duree_h', 'nom_groupe', 'intervenant', 'commentaire']].sort_values(by=['type', 'numero_ordre'], ascending=[False, False])
 
-    if selected_seance_type and selected_promotion:
-        df = df[(df['id_module'] == selected_module) &
-                (df['id_seance_type'] == selected_seance_type) &
-                (df['id_promo'] == selected_promotion)][['id_session', 'type', 'duree_h', 'nom_groupe', 'numero_ordre', 'intervenant', 'commentaire']].sort_values(by=['type', 'numero_ordre'], ascending=[False, False])
-    elif selected_seance_type:
-        df = df[(df['id_module'] == selected_module) &
-                (df['id_seance_type'] == selected_seance_type)][['id_session', 'type', 'numero_ordre', 'duree_h', 'nom_groupe', 'intervenant', 'commentaire']].sort_values(by=['type', 'numero_ordre'], ascending=[False, False])
-    elif selected_promotion:
-        df = df[(df['id_module'] == selected_module) &
-                (df['id_promo'] == selected_promotion)][['id_session', 'type', 'duree_h', 'nom_groupe', 'numero_ordre', 'intervenant', 'commentaire']].sort_values(by=['type', 'numero_ordre'], ascending=[False, False])
+        dfi = app_tools.get_explicit_keys("LNM_enseignant")
+        intervenant_options = [{'label': row['ExplicitSecondaryK'], 'value': row['id']} for _, row in dfi.iterrows()]
+
+        table_session = dash_table.DataTable(
+            id='table_session',
+            columns=[{"name": i, "id": i}
+                     for i in df.columns] + [{"name": 'nouvel_intervenant', "id": 'nouvel_intervenant', "editable": True, "presentation": "dropdown", }],  # columns must be defined so that DataTable be editable
+            data=df.to_dict('records'),
+            sort_action='native',
+            sort_mode="multi",
+            row_deletable=False,
+            dropdown={
+                "nouvel_intervenant": {
+                    "options": intervenant_options,
+                    "clearable":True,
+                }
+            },
+            style_cell_conditional=[
+                {'if': {'column_id': 'id_session', },
+                 'display': 'None', }],
+        )
     else:
-        df = df[(df['id_module'] == selected_module)][
-            ['id_session', 'type', 'numero_ordre', 'duree_h', 'nom_groupe', 'intervenant', 'commentaire']].sort_values(by=['type', 'numero_ordre'], ascending=[False, False])
-
-    dfi = app_tools.get_explicit_keys("LNM_enseignant")
-    intervenant_options = [{'label': row['ExplicitSecondaryK'], 'value': row['id']} for _, row in dfi.iterrows()]
-
-    table_session = dash_table.DataTable(
-        id='table_session',
-        columns=[{"name": i, "id": i}
-                 for i in df.columns] + [{"name": 'nouvel_intervenant', "id": 'nouvel_intervenant', "editable": True, "presentation": "dropdown", }],  # columns must be defined so that DataTable be editable
-        data=df.to_dict('records'),
-        sort_action='native',
-        sort_mode="multi",
-        row_deletable=False,
-        dropdown={
-            "nouvel_intervenant": {
-                "options": intervenant_options,
-                "clearable":True,
-            }
-        },
-        style_cell_conditional=[
-            {'if': {'column_id': 'id_session', },
-             'display': 'None', }],
-    )
+        table_session = dash_table.DataTable()
     return [table_session]
 #
 # CallBack
@@ -199,8 +207,9 @@ def register_callbacks_edit(app):
     def update_title(id_module, user_id):
         nom_module = ""
         df = app5_module_tools.get_moduleByEnseignantId(user_id)
-        if id_module and not df.empty:
-            nom_module = df[df['id_module']==id_module][['nom_module']].drop_duplicates().iloc[0]['nom_module']
+        if not df.empty:
+            if id_module and not df.empty:
+                nom_module = df[df['id_module']==id_module][['nom_module']].drop_duplicates().iloc[0]['nom_module']
         return 'Séquençage: ' + nom_module
 
     # Création de la ligne d'ajout de séquençage
@@ -213,36 +222,39 @@ def register_callbacks_edit(app):
         if not selected_module:
             return []
         df = app5_module_tools.get_moduleSequencageByEnseignantId(user_id)[['nombre', 'type', 'duree_h', 'groupe_type', 'intervenant_principal']]
-        columns = [{"name": i, "id": i, "editable": True, "presentation": "dropdown"}
-                     if  i in ["intervenant_principal", "type", "groupe_type"]
-                     else {"name": i, "id": i, "editable": True}
-                     for i in df.columns] # columns must be defined so that DataTable be editable
-        data = {}
-        for i in df.columns:
-            data[i]= ""
-        df = app_tools.get_explicit_keys("LNM_groupe_type")
-        groupe_type_options = [{'label': row['ExplicitSecondaryK'], 'value': row['id']} for _, row in df.iterrows()]
-        df = app_tools.get_explicit_keys("LNM_enseignant")
-        intervenant_options = [{'label': row['ExplicitSecondaryK'], 'value': row['id']} for _, row in df.iterrows()]
-        df = app_tools.get_explicit_keys("LNM_seance_type")
-        seance_type_options = [{'label': row['ExplicitSecondaryK'], 'value': row['id']} for _, row in df.iterrows()]
+        if not df.empty:
+            columns = [{"name": i, "id": i, "editable": True, "presentation": "dropdown"}
+                         if  i in ["intervenant_principal", "type", "groupe_type"]
+                         else {"name": i, "id": i, "editable": True}
+                         for i in df.columns] # columns must be defined so that DataTable be editable
+            data = {}
+            for i in df.columns:
+                data[i]= ""
+            df = app_tools.get_explicit_keys("LNM_groupe_type")
+            groupe_type_options = [{'label': row['ExplicitSecondaryK'], 'value': row['id']} for _, row in df.iterrows()]
+            df = app_tools.get_explicit_keys("LNM_enseignant")
+            intervenant_options = [{'label': row['ExplicitSecondaryK'], 'value': row['id']} for _, row in df.iterrows()]
+            df = app_tools.get_explicit_keys("LNM_seance_type")
+            seance_type_options = [{'label': row['ExplicitSecondaryK'], 'value': row['id']} for _, row in df.iterrows()]
 
-        table_intervenants = dash_table.DataTable(
-            id='add_table',
-            columns= columns,
-            data=[data],
-            dropdown={
-                "type": {
-                    "options": seance_type_options
+            table_intervenants = dash_table.DataTable(
+                id='add_table',
+                columns= columns,
+                data=[data],
+                dropdown={
+                    "type": {
+                        "options": seance_type_options
+                    },
+                    "intervenant_principal": {
+                        "options": intervenant_options
+                    },
+                    "groupe_type": {
+                        "options": groupe_type_options
+                    }
                 },
-                "intervenant_principal": {
-                    "options": intervenant_options
-                },
-                "groupe_type": {
-                    "options": groupe_type_options
-                }
-            },
-        )
+            )
+        else:
+            table_intervenants = dash_table.DataTable()
         return [table_intervenants]
 
     # Ajout d'un séquençage au "clique" bouton (ajout à la BD et mise à jour de la table de séquençage)
@@ -330,13 +342,16 @@ def register_callbacks_edit(app):
     )
     def cb_check_sequencage_vs_maquette(data, id_module, user_id):
         df = app5_module_tools.check_moduleSequencage(user_id)
-        if id_module:
-            df = df[df['id_module'] == id_module][['code_module', 'ecart_CM', 'ecart_TD', 'ecart_TP']]
+        if not df.empty:
+            if id_module:
+                df = df[df['id_module'] == id_module][['code_module', 'ecart_CM', 'ecart_TD', 'ecart_TP']]
 
-        table_check = dash_table.DataTable(
-            id='check_table',
-            data=df.to_dict('records'),
-        )
+            table_check = dash_table.DataTable(
+                id='check_table',
+                data=df.to_dict('records'),
+            )
+        else:
+            table_check = dash_table.DataTable()
         return [table_check]
 
     #
@@ -352,7 +367,9 @@ def register_callbacks_edit(app):
     )
     def update_filter_sequence_option(user_id, selected_module):
         df = app5_module_tools.get_moduleSequenceByEnseignantId(user_id)
-        options = [{'label': row['type'], 'value': row['id_seance_type']} for _, row in df[df['id_module'] == selected_module][['id_seance_type', 'type']].drop_duplicates().iterrows()]
+        options = []
+        if not df.empty:
+            options = [{'label': row['type'], 'value': row['id_seance_type']} for _, row in df[df['id_module'] == selected_module][['id_seance_type', 'type']].drop_duplicates().iterrows()]
         return options
 
     # Mise à jour de la table des séances (séquences) selon le module sélectionné et le type de cours
@@ -407,7 +424,9 @@ def register_callbacks_edit(app):
     )
     def update_filter_session_option(user_id, selected_module):
         df = app5_module_tools.get_moduleSessionByEnseignantId(user_id)
-        options = [{'label': row['promo'], 'value': row['id_promo']} for _, row in df[df['id_module'] == selected_module][['id_promo', 'promo']].drop_duplicates().iterrows()]
+        options = []
+        if not df.empty:
+            options = [{'label': row['promo'], 'value': row['id_promo']} for _, row in df[df['id_module'] == selected_module][['id_promo', 'promo']].drop_duplicates().iterrows()]
         return options
 
     # Update session table
