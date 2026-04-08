a60f801e360f9754923954cfb24b263999b916dc
Souha <gharsallah118@gmail.com>
2026-04-08 18:34:43 +0200

    Ajout orientation par metiers

--- backend_python/user/apc_KPI_competence.py
+++ backend_python/user/apc_KPI_competence.py
@@ -7,6 +7,64 @@ from dependencies import db_request, get_current_active_user, User, SQLRequest
 logger = logging.getLogger(__name__)
 router = APIRouter()
 
+
+@router.get(
+    "/metier_ac/{id_metier:int}",
+    tags=["apc"],
+    summary="Get AC, semester and competence by metier",
+    description="Return the list of AC, semester and competence for a metier",
+)
+def get_metier_ac(
+    id_metier: int,
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    request = {
+        "request": f"""
+            SELECT DISTINCT
+                metier.libelle_situation,
+                metier.id_situation_professionnelle,
+                semestre.id_semestre,
+                semestre.semestre,
+                ac.id_apprentissage_critique,
+                ac.libelle_apprentissage,
+                competence.id_competence,
+                competence.code_competence
+            FROM APC_situation_professionnelle metier
+            JOIN APC_competence competence
+                ON metier.id_competence = competence.id_competence
+            JOIN APC_niveau niveau
+                ON niveau.id_competence = competence.id_competence
+            JOIN APC_apprentissage_critique ac
+                ON ac.id_niveau = niveau.id_niveau
+            JOIN APC_apprentissage_critique_as_module acmodule
+                ON acmodule.id_apprentissage_critique = ac.id_apprentissage_critique
+            JOIN MAQUETTE_module module
+                ON module.id_module = acmodule.id_module
+            JOIN LNM_semestre semestre
+                ON semestre.id_semestre = module.id_semestre
+            WHERE metier.id_situation_professionnelle = {id_metier}
+        """,
+        "allowedRolesRequester": ["user"],
+    }
+
+    return db_request(current_user, SQLRequest(**request))
+
+@router.get("/metier/",
+            tags=["apc"],
+            summary="Students",
+            description="Return the list of situation_professionnelle ",)
+def get_metier(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    request = {
+        "request" : """
+                    SELECT metier.id_situation_professionnelle,metier.libelle_situation 
+                    FROM APC_situation_professionnelle metier
+                    """,
+        "allowedRolesRequester" : ["user"],
+    }
+    return db_request(current_user, SQLRequest(**request))
+
 @router.get("/acp_kpi/",
             tags=["apc"],
             summary="nb competence",
--- front_DashPlotly/main.py
+++ front_DashPlotly/main.py
@@ -64,6 +64,7 @@ def import_apps():
     from apc_dash.apc20_KPI import  apc_layout,register_callbacks as register_callbacks_apc
     from apc_dash.apc20_competence_ac import apc20_competence_ac_layout, register_callbacks as register_callbacks_apc20
     from apc_dash.app_reseaupolytech_etudiant import polytech_analysis_layout, register_reseau_polytech_callbacks
+    from apc_dash.apc20_metier_orientation import apc20_metier_orientation_layout,register_callbacks as register_callbacks_apc20_orientation_metier
     return {
         'app2': (app2_layout, register_callbacks_app2),
         'app3_administratif': (app3_administratif_layout, register_callbacks_app3_administratif),
@@ -87,6 +88,7 @@ def import_apps():
         'apc20_KPI_competence':(apc_layout,register_callbacks_apc),
         'apc20_learnagement': (apc20_competence_ac_layout, register_callbacks_apc20),
         'app15_etudiant':(polytech_analysis_layout,register_reseau_polytech_callbacks),
+        'apc20_orientation_metier':(apc20_metier_orientation_layout,register_callbacks_apc20_orientation_metier),
     }#apc20_etudiant
 
 LOGO = "https://placehold.co/100x100"
@@ -103,6 +105,8 @@ menu_items = {
         ('APC_heatmap', 'apc20_heatmap_apc'),
         ('APC_kpi','apc20_KPI_competence'),
         ('APC', 'apc20_learnagement'),
+        ('Réseau Polytech','app15_etudiant'),
+        ('orientation par metier','apc20_orientation_metier'),
     ],
     'enseignant': [
         ('Vue modules', 'app5_enseignant_view'),
@@ -113,8 +117,10 @@ menu_items = {
         ('Charge de travail', 'app7_enseignant'),
         ('Tutorat stages', 'app10_enseignant'),
         ('APC_heatmap', 'apc20_heatmap_apc'),
-        ('APC_kpi','apc20_KPI_competence'),
+        #('APC_kpi','apc20_KPI_competence'),
         ('APC', 'apc20_learnagement'),
+        ('Réseau Polytech','app15_etudiant'),
+        ('orientation par metier','apc20_orientation_metier'),
     ],
     'etudiant': [
         ('Compétences', 'app2'),
@@ -128,6 +134,7 @@ menu_items = {
         ('APC_kpi','apc20_KPI_competence'),
         ('APC', 'apc20_learnagement'),
         ('Réseau Polytech','app15_etudiant'),
+        ('orientation par metier','apc20_orientation_metier'),
     ]
 }
 
